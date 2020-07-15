5bacb2d126aeefcce1ac13fb7f50acaae3cd23b2
Trefoil <zjb2949@126.com>
2020-07-15 16:04:05 +0800

    init-blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: "vuepress",
+  description: "vuepress-des",
+  head:[
+    [
+      'link',{icon:''}
+    ]
+  ],
+  themeConfig:{
+    nav: [
+      {text: "主页", link: "/"      },
+      { text: "node", link: "/node/" },
+      { text: "前端", link: "/webframe/"},
+      { text: "数据库", link: "/database/"   },
+      { text: "android", link: "/android/"   },
+      { text: "面试问题", link: "/interview/" }
+    ]
+  }
+}
